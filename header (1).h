a0ff8ca3c4793318c7b522f10ea57b715542bdb2
HassiaT <hassia.tamimou@gmail.com>
2017-12-13 16:24:57 +0100

    Add files via upload
    Header File for Cuda project: pricing of options

---
+++ header (1).h
@@ -0,0 +1,7 @@
+#if !defined(HEADER_H)
+#define HEADER_H
+#include <stdio.h>
+#include <stdlib.h>
+
+extern "C" void do_master(int,int,int,int,double*,double*);
+#endif /* if !defined(_HEADER_H_) */
